5994fb5de3e131bad41484796daa1d75010ef729
Declan Harrop <dh@38co.io>
2021-01-15 13:46:01 +0000

    Addded info to Help page

--- pages/Stories/Awards.js
+++ pages/Stories/Awards.js
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
 import ImageHeader from '../../components/ImageHeader';
 import Layout from '../../elements/Layout';
+import AwardPhotos from '../../public/Awards/index';
 
 const Wrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  max-width: 1000px;
+  max-width: 1200px;
   margin: 0 auto 10vh auto;
   padding: 20px;
+
+  img {
+    max-width: 360px;
+  }
 `;
 
 export default function Awards() {
@@ -22,7 +27,9 @@ export default function Awards() {
       <ImageHeader height="30vh" text="Awards" />
       <Wrap>
         <div className="item">
-          <img src="" alt="" />
+          {AwardPhotos.map((item, i) => (
+            <img key={i} src={item.img} alt="" />
+          ))}
         </div>
       </Wrap>
     </Layout>
---
+++ public/Awards/index.js
@@ -0,0 +1,9 @@
+const AwardPhotos = [
+  { img: '/Awards/Award1.jpg' },
+  { img: '/Awards/Award2.jpg' },
+  { img: '/Awards/Award3.jpg' },
+  { img: '/Awards/Award4.jpg' },
+  { img: '/Awards/Award5.jpg' },
+];
+
+export default AwardPhotos;
