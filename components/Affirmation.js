8e4f24f56e183436275b4bfcf4ff95fd2df19415
Declan Harrop <dh@38co.io>
2021-01-20 18:50:39 +0000

    quote

--- components/Affirmation.js
+++ components/Affirmation.js
@@ -4,11 +4,16 @@ const Wrap = styled.section`
   margin: 120px auto;
   padding: 20px;
   max-width: 800px;
-  img {
+  video {
     margin: 30px 0 0 0;
-    max-width: 400px;
+    max-width: 560px;
     border-radius: 4px;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  }
+  @media only screen and (max-width: 600px) {
+    video {
+      max-width: 300px;
+    }
   }
 `;
 
@@ -16,7 +21,7 @@ export default function Affirmation() {
   return (
     <Wrap>
       <h2>Take a screenshot for today's kind thought</h2>
-      <img src="/img/aff/aff1.jpg" alt="" />
+      <video src="/aff.mp4" type="video/mp4" autoPlay loop muted />
     </Wrap>
   );
 }
